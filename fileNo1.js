83002452f2ebe9cc89a0154a50c4df3ae544d8ed
muhid <shaikhmuhid165@gmail.com>
2023-05-17 20:15:29 +0530

    added a print-pattern program

--- fileNo1.js
+++ fileNo1.js
@@ -1,1 +1,15 @@
 console.log("hello js once again!!!!!")
+
+let mystring = "";
+let n = 4;
+for(let i = 0;i <= n;i++){
+    for(let j = 0;j <= n;j++){
+        mystring +=" #";
+        // console.log(" internal loop!!!")
+    }
+    // console.log("external loop!!!")
+    mystring += "\n";
+}
+console.log(mystring);
+
+
